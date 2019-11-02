3f4467bc0f02cd08d200f83b1b30ff0e6c35bec5
jothi <hca.try@gmail.com>
2019-11-02 17:38:17 +0530

    hit added

--- multi.py
+++ multi.py
@@ -1,17 +1,25 @@
+# header file imported
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
+# creating a file name
 file = "Details.csv"
-f = open(file, "w")
-Headers = "Name,Price,Rating\n"
-f.write(Headers)
+f = open(file, "w") #open the file
+Headers = "Name\n" #heading part
+f.write(Headers) #inserting the heading
+
+# loop for multi page open
 for page in range(0,1):
-    url = "https://stackoverflow.com/users?page=1&tab=reputation&filter=week".format(page)
+    # opening the url
+    url = "https://stackoverflow.com/users?page="+page+"&tab=reputation&filter=week".format(page)
     html = urlopen(url)
+    # scraping using bs4
     soup = BeautifulSoup(html,"html.parser")
+    # finding the name of the user inside the class file
     Title = soup.find_all("div", {"class":"user-details"})
-    print(len(Title))
+    print(len(Title)) #total lenth of the user in the page
     
+    # loop for getting the name 
     for i in Title:
         try:
             name = i.find('a').get_text()
@@ -19,6 +27,5 @@ for page in range(0,1):
             name_list = (name)
             f.write(name_list)
             f.write("\n")    
-            # f.write("{}".format(name).replace(",","|")+ ",{}".format(price)+ ",{}".format(rating).replace(",", " ")+ "\n")
         except: AttributeError
 f.close()
